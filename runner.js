78353a787442eb20479c9d56d6c298858e0d14f7
Andraž Dobnikar <andraz.dobnikar@signapps.si>
2018-07-25 00:10:18 +0200

    Log conditions.

--- runner.js
+++ runner.js
@@ -12,6 +12,9 @@ function checkConditions(task, parameters) {
 
 	let match = true;
 
+	console.log("TASK CONDITIONS", task.conditions);
+	console.log("HOOK DATA", parameters);
+
 	for (let name in task.conditions) {
 		let condition = task.conditions[name];
 
